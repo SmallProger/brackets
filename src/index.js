6a74155c75ee386dd95bc786105d05c51ba9f1a3
Kirill Zubkov <kirya.zubkov.98@list.ru>
2022-02-24 23:34:57 +0300

    feat: final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let findedIndexes = new Set();
+
+  str = str.split("");
+  for (let i = 0; i < str.length; i++) {
+    for (let item of bracketsConfig) {
+      if (str[i] == item[0] && !findedIndexes.has(i)) {
+        let index = str.findIndex((elem, index) => {
+          if (!findedIndexes.has(index) && elem == item[1] && index > i) {
+            return true;
+          } else return false;
+        });
+        if (index == -1) return false;
+        findedIndexes.add(index);
+      }
+    }
+  }
+  if (findedIndexes.size != Math.floor(str.length / 2)) return false;
+  return true;
+};
